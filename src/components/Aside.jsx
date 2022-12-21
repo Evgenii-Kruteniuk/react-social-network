0fc64fea6ed88b903a4ec7f6d35d1618d3d9a53b
Evgenii-Kruteniuk <muster00100@yandex.ru>
2022-12-21 14:23:22 +0300

    21.12.22

---
+++ src/components/Aside.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Aside() {
+  return (
+    <aside className="aside">
+      <div>
+        <a href="">Profile</a>
+      </div>
+      <div>
+        <a href="">Messages</a>
+      </div>
+      <div>
+        <a href="">News</a>
+      </div>
+      <div>
+        <a href="">Music</a>
+      </div>
+      <div>
+        <a href="">Settings</a>
+      </div>
+    </aside>
+  );
+}
+
+export default Aside;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="header">
+      <img src="https://freelance.ru/img/portfolio/pics/00/34/10/3412097.jpg" />
+    </div>
+  );
+}
+
+export default Header;
